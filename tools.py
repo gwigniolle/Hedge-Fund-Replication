02913011cf9514404c8fd9aec116c92e0e2f13ff
Guillaume Wigniolle <guillaume.wigniolle@student.ecp.fr>
2017-11-21 14:09:52 +0100

    Financial data removed

--- tools.py
+++ tools.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.optimize import minimize
+from scipy import stats
 
 
 def make_ER(price, rate):
@@ -82,6 +83,17 @@ def ols_regression(df_y, df_x, sample_length: int, frequency: int, boundaries=(-
         df_weight.loc[end] = res.x
 
     return df_weight
+
+
+def make_stats(df_price):
+    df_return = df_price.pct_change().dropna()
+    stats.describe(df_return)
+    t_tstat, p_tstat = stats.ttest_rel(df_return.iloc[:,0], df_return.iloc[:, 1])  # T-test
+    t_KS, p_KS = stats.ks_2samp(df_return.iloc[:,0], df_return.iloc[:, 1])  # KS -> p petit pas la meme distri
+    tau, p_tau = stats.kendalltau(df_return.iloc[:,0], df_return.iloc[:, 1])  # Tau de Kendall
+
+    return stats.describe(df_return),"t test: t = %g  p = %g" % (t_tstat, p_tstat), \
+           "KS test: t = %g  p = %g" % (t_KS, p_KS), "KendallTau: t = %g  p = %g" % (tau, p_tau)
 
 
 if __name__ == "__main__":

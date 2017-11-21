cf9679f99cf191af6840221a55ce8314be882d63
Guillaume Wigniolle <guillaume.wigniolle@student.ecp.fr>
2017-11-21 16:11:21 +0100

    Lasso and ER regression

--- tools.py
+++ tools.py
@@ -8,6 +8,13 @@ from scipy import stats
 
 
 def make_ER(price, rate):
+
+    """
+    :param price: pd.DataFrame containing the prices of the ticker
+    :param rate: pd.Series containing the rate prices
+    :return:
+    """
+
     dates = price.index
     price_ER = pd.DataFrame(index=dates, columns=price.columns)
     price_ER.iloc[0] = 1.
@@ -33,8 +40,6 @@ def make_track(df_price, df_weight, tc=0):
 
     index = df_price.index
     reweight_index = df_weight.index
-
-    print((index[1]-index[0]).days)
 
     n = len(index)
     shares = (df_weight / df_price).iloc[0]
@@ -81,6 +86,100 @@ def ols_regression(df_y, df_x, sample_length: int, frequency: int, boundaries=(-
         res = minimize(loss, z0, method='SLSQP', constraints=cons, bounds=bounds)
 
         df_weight.loc[end] = res.x
+
+    return df_weight
+
+
+def ols_regression_ER(df_y, df_x, sample_length: int, frequency: int, boundaries=(-np.inf, np.inf),
+                     weight_sum=np.nan):
+    """
+        This regression can only be used in excess return as the weight sum does not equal 0
+
+    :param df_y:
+    :param df_x:
+    :param sample_length:
+    :param frequency:
+    :param boundaries:
+    :param weight_sum:
+    :return:
+    """
+
+    index = df_y.index.copy()
+    n, m = df_x.shape
+
+    df_weight = pd.DataFrame(columns=df_x.columns)
+
+    for i in range((n - sample_length)//frequency):
+
+        start = index[i*frequency]
+        end = index[i*frequency + sample_length]
+
+        x = df_x.loc[start:end]
+        std_x = x.std(axis=0).replace(0, np.nan)
+        x = (x/std_x).replace(np.nan, 0).values
+
+        y = df_y.loc[start:end]
+        std_y = y.std(axis=0).replace(0, np.nan)
+        y = (y/std_y).replace(np.nan, 0).values
+
+        def loss(z):
+            return np.sum((np.dot(x, z)-y)**2)
+
+        cons = ({'type': 'eq',
+                 'fun': lambda z: np.sum(z) - weight_sum}) if not np.isnan(weight_sum) else ()
+        bounds = [boundaries]*m
+        z0 = np.zeros([m, 1])
+
+        res = minimize(loss, z0, method='SLSQP', constraints=cons, bounds=bounds)
+
+        df_weight.loc[end] = std_y.iloc[0]*res.x/std_x
+
+    return df_weight
+
+
+def lasso_regression_ER(df_y, df_x, sample_length: int, frequency: int, lambda_:float, boundaries=(-np.inf, np.inf),
+                        weight_sum=np.nan):
+    """
+        This regression can only be used in excess return as the weight sum does not equal 0
+
+    :param df_y:
+    :param df_x:
+    :param sample_length:
+    :param frequency:
+    :param boundaries:
+    :param weight_sum:
+    :return:
+    """
+
+    index = df_y.index.copy()
+    n, m = df_x.shape
+
+    df_weight = pd.DataFrame(columns=df_x.columns)
+
+    for i in range((n - sample_length) // frequency):
+        start = index[i * frequency]
+        end = index[i * frequency + sample_length]
+
+        x = df_x.loc[start:end]
+        std_x = x.std(axis=0).replace(0, np.nan)
+        x = (x / std_x).replace(np.nan, 0).values
+
+        y = df_y.loc[start:end]
+        std_y = y.std(axis=0).replace(0, np.nan)
+        y = (y / std_y).replace(np.nan, 0).values
+
+        def loss(z):
+            return np.sum((np.dot(x, z) - y)**2) + lambda_*n*np.sum(np.abs(z))
+
+        cons = ({'type': 'eq',
+                 'fun': lambda z: np.sum(z) - weight_sum}) if not np.isnan(weight_sum) else ()
+
+        bounds = [boundaries] * m
+        z0 = np.zeros([m, 1])
+
+        res = minimize(loss, z0, method='SLSQP', constraints=cons, bounds=bounds)
+
+        df_weight.loc[end] = std_y.iloc[0] * res.x / std_x
 
     return df_weight
 
